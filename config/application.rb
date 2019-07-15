a88c833674612df1886f4f859ba982bb0fc2ff41
keiko kamataki <keiko0411kamataki@gmail.com>
2019-07-15 05:34:56 +0000

    Finsh top-page

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module AttendanceApp
     # Initialize configuration defaults for originally generated Rails version.
     
     config.load_defaults 5.1
-　　config.time_zone = 'Asia/Tokyo' # この１行をここに追加します。
+    config.time_zone = 'Asia/Tokyo' # この１行をここに追加します。
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
